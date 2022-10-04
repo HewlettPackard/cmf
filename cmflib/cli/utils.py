574d77508608ceff8a298979e729f4808ab1a3a0
varkha-d-sharma <varkhadsharma@gmail.com>
2022-10-04 20:02:29 +0530

    Reverted some changes as they were needed to keep this PR focused only on Pull command

--- cmflib/cli/utils.py
+++ cmflib/cli/utils.py
@@ -1,13 +1,4 @@
-import yaml
-
-
 def fix_subparsers(subparsers):
     subparsers.required = True
     subparsers.dest = "cmd"
 
-
-def read_cmf_config():
-    with open("../../cmfconfig/config.yaml", "r") as yamlfile:
-        data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-        print("Read Successful")
-    return data
