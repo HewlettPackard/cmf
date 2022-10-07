f4d040a9e93f069dae30a3e2d7a57e9b90724333
abhinav <first.second@corp.org>
2022-10-07 17:57:12 +0530

    adding  artifact/__init__.py

---
+++ cmflib/commands/artifact/__init__.py
@@ -0,0 +1,29 @@
+import argparse
+
+from cmflib.commands.artifact import pull
+from cmflib.cli.utils import *
+
+SUB_COMMANDS = [
+    pull
+]
+
+
+def add_parser(subparsers, parent_parser):
+    ARTIFACT_HELP = "Command for artifact pull/push"
+
+    artifact_parser = subparsers.add_parser(
+        "artifact",
+        parents=[parent_parser],
+        description="This command is used pull/push Artifact",
+        help=ARTIFACT_HELP,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    artifact_subparsers = artifact_parser.add_subparsers(
+        dest="cmd",
+        help="Use `cmf artifact CMD --help` for " "command-specific help.",
+    )
+
+    fix_subparsers(artifact_subparsers)
+    for cmd in SUB_COMMANDS:
+        cmd.add_parser(artifact_subparsers, parent_parser)
