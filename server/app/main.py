40e57d1279d60e5f8727ac6d25a78dc8c533683a
abhinav <first.second@corp.org>
2022-10-27 00:51:17 +0530

    adding server api for gui that displays executions

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI,Request
 from server.app.mlmd import merge_mlmd
-
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from get_executions import index
+from pathlib import Path
+import json
 app = FastAPI()
 
+BASE_PATH=Path(__file__).resolve().parent
+templates=Jinja2Templates(directory=str(BASE_PATH/'template'))
 
 @app.get("/")
 def read_root():
@@ -19,3 +25,11 @@ async def mlmd_push(info: Request):
         'data':req_info
     }
 
+
+@app.get("/display_exec",response_class=HTMLResponse)
+async def display_exec(request: Request):
+    # with open('execution.json','r') as f:
+    #     data=json.load(f)
+    #
+    df=index("/home/abhinavchobey/server_demo/cmf/pipeline_data/example_get_started/mlmd")
+    return templates.TemplateResponse('index.html',{'request':request,'df':df})
