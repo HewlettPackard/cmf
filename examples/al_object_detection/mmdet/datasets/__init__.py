44febe70d9d78ffcf9780e99f2b6fcdf376affc9
Annmary Justine K <annmary.roy@hpe.com>
2022-12-19 11:59:44 -0600

    Example custome dataset with single class

--- examples/al_object_detection/mmdet/datasets/__init__.py
+++ examples/al_object_detection/mmdet/datasets/__init__.py
@@ -8,12 +8,13 @@ from .deepfashion import DeepFashionDataset
 from .lvis import LVISDataset, LVISV1Dataset, LVISV05Dataset
 from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
 from .voc import VOCDataset
+from .gray import GrayDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
-    'VOCDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
+    'VOCDataset', 'GrayDataset', 'CityscapesDataset', 'LVISDataset', 'LVISV05Dataset',
     'LVISV1Dataset', 'GroupSampler', 'DistributedGroupSampler',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
