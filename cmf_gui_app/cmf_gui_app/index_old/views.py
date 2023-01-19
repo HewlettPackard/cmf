19a131725c9acf5780dfb24434a55d15d0cec038
abhinav <first.second@corp.org>
2023-01-19 12:14:23 +0530

    removed reference of mlmd.py from server/app/main.py and deleted cmf_gui_app folder

--- cmf_gui_app/cmf_gui_app/index_old/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render  
-#importing loading from django template  
-from django.template import loader  
-# Create your views here.  
-from django.http import HttpResponse  
-def index(request):  
-   template = loader.get_template('index.html') # getting our template  
-   return HttpResponse(template.render()) 
