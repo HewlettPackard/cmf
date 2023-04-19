654bf88191405906c70d23cc7698528600f189ef
Varkha Sharma <varkhadsharma@gmail.com>
2023-04-18 18:19:50 -0700

    Added changes to retrieve user, pssword and uri for neo4j

--- cmflib/cmf.py
+++ cmflib/cmf.py
@@ -73,8 +73,8 @@ class Cmf:
     if os.path.exists(cmf_config):
         attr_dict = CmfConfig.read_config(cmf_config)
         __neo4j_uri = attr_dict.get("neo4j-uri", "")
-        __neo4j_user = attr_dict.get("neo4j-password", "")
-        __neo4j_password = attr_dict.get("neo4j-user", "")
+        __neo4j_password = attr_dict.get("neo4j-password", "")
+        __neo4j_user = attr_dict.get("neo4j-user", "")
 
     def __init__(self, filename: str = "mlmd",
                  pipeline_name: str = "", custom_properties: t.Optional[t.Dict] = None,
