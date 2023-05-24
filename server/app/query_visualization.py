62dd1ec50c19bb25ffd130f21ed7c4eb26cae874
Abhinav Chobey <chobey@abhinav-cmf-machine-2.labs.hpecorp.net>
2023-05-24 06:32:25 -0700

    adding lineage functionality

--- server/app/query_visualization.py
+++ server/app/query_visualization.py
@@ -1,10 +1,15 @@
 import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
 import pandas as pd
 from cmflib import cmfquery
 import networkx
 import matplotlib.pyplot as plt
 import dvc
+import json
+import random
 import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def query_visualization(mlmd_path, pipeline_name):
@@ -12,31 +17,74 @@ def query_visualization(mlmd_path, pipeline_name):
     query = cmfquery.CmfQuery(mlmd_path)
     stages = query.get_pipeline_stages(pipeline_name)
     list_all_artifacts = []
+    list_of_edges = []
+    temp = []
+    temp1 = []
+    list_all_artifacts_with_chash = []
+    artifact_name_type_dict = {}
     for stage in stages:
         executions = query.get_all_executions_in_stage(stage)
         artifacts = query.get_all_artifacts_for_execution(executions.iloc[0]["id"])
+        artifact_type_list = [i for i in artifacts["type"]]
+        artifact_name_list = [i.split(":")[0].split("/")[-1] for i in artifacts["name"]]
+        artifact_name_type_dict.update(
+            dict(zip(artifact_name_list, artifact_type_list))
+        )
         for name in artifacts["name"]:
             list_all_artifacts.append(name)
-    for artifact in list(set(list_all_artifacts)):
-        g.add_node(artifact.split(":")[0])
+    list_all_artifacts_with_chash = list_all_artifacts
+
+    for artifact in list(set(list_all_artifacts_with_chash)):
+        temp1.append(artifact.split(":")[0].split("/")[-1])
+        immediate_child_artifacts = query.get_one_hop_child_artifacts(artifact)
+        if len(immediate_child_artifacts) != 0:
+            temp1.extend(
+                [
+                    i.split(":")[0].split("/")[-1]
+                    for i in list(set(immediate_child_artifacts["name"]))
+                ]
+            )
+    list_all_artifacts = list(set(temp1))
+    g.add_nodes_from(list_all_artifacts)
+    for artifact in list(set(list_all_artifacts_with_chash)):
         immediate_child_artifacts = query.get_one_hop_child_artifacts(artifact)
         if immediate_child_artifacts.empty == True:
             pass
         else:
-            for i in immediate_child_artifacts["name"]:
-                g.add_edge(artifact.split(":")[0], i.split(":")[0])
-    pos = networkx.spiral_layout(g, scale=4)
-    networkx.draw_networkx(
+            for i in list(set(immediate_child_artifacts["name"])):
+                g.add_edge(
+                    (artifact.split(":")[0].split("/")[-1]),
+                    i.split(":")[0].split("/")[-1],
+                )
+    nx.nx_agraph.write_dot(g, "Test.dot")
+    node_color_list = []
+    for i in list_all_artifacts:
+        if i in artifact_name_type_dict.keys():
+            if artifact_name_type_dict[i] == "Dataset":
+                node_color_list.append("orange")
+            elif artifact_name_type_dict[i] == "Model":
+                node_color_list.append("skyblue")
+            else:
+                node_color_list.append("green")
+        else:
+            node_color_list.append("red")
+    pos = graphviz_layout(g, prog="dot", args="-Grankdir=LR")
+    nx.draw(
         g,
         pos,
         font_size=10,
         arrowsize=20,
-        node_color="skyblue",
-        node_size=400,
+        node_color=node_color_list,
+        node_size=1000,
         with_labels=True,
     )
+
+    img_path = "lineage" + str(random.randint(1, 20)) + ".jpg"
     plt.savefig(
-        "./data/static/lineage.png", dpi=899, pad_inches=10
+        "./data/static/" + img_path,
+        bbox_inches="tight",
+        pad_inches=1,
+        orientation="landscape",
     )
-    return True
-    # query_visualization("/home/abhinavchobey/hpserver/cmf/local/example-get-started/mlmd",'Test-env' )
+    plt.figure().clear()
+    return img_path
