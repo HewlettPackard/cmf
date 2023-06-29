b2fd6f1c87531ce8f5cc7f9d241890f4431164bb
annmary-roy <82071576+annmary-roy@users.noreply.github.com>
2023-06-29 09:24:25 -0600

    Update execution, model is treated similar to dataset, uuid changes , fix for duplicate execution (#109)
    * adding changes for the update functionality
    
    * changes to the get_stages call
    
    * added examples for update_execution call
    
    * additional fixes
    
    * not creating metrics again and again
    
    * Reverted the changes of my env from docker files
    
    * Modified the readme
    
    * Models - irrespective of whether a model is input or output it will be associated only with a single object in mlmd
    
    * update executions - added uuid for each execution
    
    * removed unwanted print statemnets
    
    * Removed unnecessary print statements
    
    ---------
    
    Co-authored-by: Annmary justine <annmary.roy@hpe.com>

---
+++ examples/example-get-started/test_execution_update/test_execution_update_method_2.py
@@ -0,0 +1,19 @@
+from cmflib import cmf
+from cmflib import cmfquery
+
+query = cmfquery.CmfQuery("mlmd")
+if len(query.get_pipeline_names()) == 0:
+    print("This script needs to be run after test_execution_update.py")
+    exit()
+pipeline_name = query.get_pipeline_names()[0]
+print(pipeline_name)
+
+metawriter = cmf.Cmf("mlmd", "test-execution-update")
+_ = metawriter.create_context("Train")
+_ = metawriter.create_execution("Train-execution",{"name":"test-1"}, create_new_execution=False)
+
+_ = metawriter.log_dataset("artifacts/data.xml.gz", "input", custom_properties={"user-metadata1": "metadata_value"})
+_ = metawriter.log_execution_metrics("metrics-1", {"auc":0.4})
+
+metawriter.finalize()
+print("Test executed successfully")
