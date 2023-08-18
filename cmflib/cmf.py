9ad7c74fc1d63beed169282e711299f33ef3dc8d
Abhinav Chobey <chobey@abhinav-cmf-hpe.labs.hpecorp.net>
2023-08-18 07:21:50 -0700

    cmf init commands created outside cmf.py

--- cmflib/cmf.py
+++ cmflib/cmf.py
@@ -57,12 +57,7 @@ from cmflib.cmf_commands_wrapper import (
     mt_push,
     mt_pull,
     arti_pull,
-    arti_push,
-    cmf_cmd_init,
-    init_local,
-    init_minioS3,
-    init_amazonS3,
-    init_sshremote
+    arti_push
 )
 
 class Cmf:
@@ -1399,102 +1394,6 @@ class Cmf:
     def artifact_push(self):
         """Push artifacts to initialized repository"""
         output = arti_push()
-        return output
-
-    def cmf_init_show(self):
-        """Check whether repository is initialized(local,minioS3,amazonS3,sshremote)"""
-        output = cmf_cmd_init()
-        return output
-
-    def cmf_init_local(
-        self,
-        path: str,
-        git_remote_url: str,
-        cmf_server_url: str = "",
-        neo4j_user: str = "",
-        neo4j_password: str = "",
-        neo4j_uri: str = "",
-    ):
-        """Initialize local repository"""
-        output = init_local(
-            path, git_remote_url, cmf_server_url, neo4j_user, neo4j_password, neo4j_uri
-        )
-        return output
-
-    def cmf_init_minio(
-        self,
-        url: str,
-        endpoint_url: str,
-        access_key_id: str,
-        secret_key: str,
-        git_remote_url: str,
-        cmf_server_url: str = "",
-        neo4j_user: str = "",
-        neo4j_password: str = "",
-        neo4j_uri: str = "",
-    ):
-        """Initialize minioS3 repository"""
-        output = init_minioS3(
-            url,
-            endpoint_url,
-            access_key_id,
-            secret_key,
-            git_remote_url,
-            cmf_server_url,
-            neo4j_user,
-            neo4j_password,
-            neo4j_uri,
-        )
-        return output
-    
-    def cmf_init_amazon(
-        self,
-        url: str,
-        access_key_id: str,
-        secret_key: str,
-        git_remote_url: str,
-        cmf_server_url: str = "",
-        neo4j_user: str = "",
-        neo4j_password: str = "",
-        neo4j_uri: str = "",
-    ):
-        """Initialize amazonS3 repository"""
-        output = init_amazonS3(
-            url,
-            access_key_id,
-            secret_key,
-            git_remote_url,
-            cmf_server_url,
-            neo4j_user,
-            neo4j_password,
-            neo4j_uri,
-        )
-        return output
-
-    def cmf_init_sshremote(
-        self,
-        path: str,
-        user: str,
-        port: int,
-        password: str,
-        git_remote_url: str,
-        cmf_server_url: str = "",
-        neo4j_user: str = "",
-        neo4j_password: str = "",
-        neo4j_uri: str = "",
-    ):
-        """Initialize sshremote repository"""
-        output = init_sshremote(
-            path,
-            user,
-            port,
-            password,
-            git_remote_url,
-            cmf_server_url,
-            neo4j_user,
-            neo4j_password,
-            neo4j_uri,
-        )
         return output
 
     def create_dataslice(self, name: str) -> "Cmf.DataSlice":
---
+++ examples/example-get-started/src/test_pull.py
@@ -0,0 +1,43 @@
+from cmflib.cmf import Cmf
+import os
+from cmflib.cli.utils import find_root
+from cmflib.utils.cmf_config import CmfConfig
+import requests
+
+def data_pull(pipeline_name,file_path=""):
+    server=0
+
+    cmf = Cmf(pipeline_name)
+    
+    cmfconfig = os.environ.get("CONFIG_FILE",".cmfconfig")
+
+    # find root_dir of .cmfconfig
+    output = find_root(cmfconfig)
+
+    # in case, there is no .cmfconfig file
+    if output.find("'cmf' is  not configured") != -1:
+        return output
+
+    config_file_path = os.path.join(output, cmfconfig)
+
+    attr_dict = CmfConfig.read_config(config_file_path)
+    url = attr_dict.get("cmf-server-ip", "http://127.0.0.1:80")
+    try:
+        server = requests.get(url)
+        if server.status_code!=200:
+            print("Server is down !")
+            return 
+    except:
+        print("Server is down !")
+        return 
+    meta=cmf.metadata_pull(file_path)
+    if file_path=="":
+        file_path="./mlmd"
+    else:
+        file_path=file_path+"/mlmd"
+    cmf = Cmf(file_path,pipeline_name)
+    arti=cmf.artifact_pull()
+
+    
+if __name__ == '__main__':
+    data_pull(pipeline_name="Testenv")
