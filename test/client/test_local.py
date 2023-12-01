cc3056b14e2abb0e909bbdbf79a7eeb96ca8e183
Varkha Sharma <varkhadsharma@gmail.com>
2023-12-01 07:26:46 -0800

    resolved the local files download issue

--- test/client/test_local.py
+++ test/client/test_local.py
@@ -12,7 +12,8 @@ def test_cmf_init_show():
 
 def test_cmf_init_local(cmf_server_url):
     print("-------------------------------Test Case Name: cmf init local ----------------------------------")
-    path = f"{os.getcwd()}/local-storage"
+    # local-storage can't be in the same folder where dvc was initialised
+    path = f"{os.getcwd()}/../local-storage"
     _ = cmf.cmf_init(type="local", path=path, git_remote_url="https://github.com/hpe-user/experiment-repo.git",
                neo4j_user='neo4j', neo4j_password="xxxxxx", neo4j_uri="bolt://xx.xx.xxx.xxx:7687", cmf_server_url=cmf_server_url)
 
