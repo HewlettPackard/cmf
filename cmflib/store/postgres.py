0a9b91e9a12d255b660bb3938a80c85641ba25e8
Varkha Sharma <varkhadsharma@gmail.com>
2024-06-26 01:58:13 -0700

    Made changes to make mlmd postgres compataible

--- cmflib/store/postgres.py
+++ cmflib/store/postgres.py
@@ -1,13 +1,12 @@
 #from cmfstore import CmfStore
 from cmflib.store.cmfstore import CmfStore
-
+from ml_metadata.proto import metadata_store_pb2 as mlpb
 from ml_metadata.metadata_store import metadata_store
 
 class PostgresStore(CmfStore):
 
     def __init__(self, config):
-
-        self.connection_config = metadata_store.ConnectionConfig() 
+        self.connection_config = mlpb.ConnectionConfig()
         self.connection_config.postgresql.host = config["host"]
         self.connection_config.postgresql.port = config["port"]
         self.connection_config.postgresql.user = config["user"]
@@ -15,5 +14,7 @@ class PostgresStore(CmfStore):
         self.connection_config.postgresql.dbname = config["dbname"]
         super().__init__(self.connection_config)
 
-    def connect(self):
-        super().connect()
+    def connect(self)-> metadata_store:
+        cmf_store = super().connect()
+        return cmf_store
+
