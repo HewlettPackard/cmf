7e96edd61b79d16fef096cc8b8289df1c7057711
Varkha Sharma <varkhadsharma@gmail.com>
2024-06-26 01:58:13 -0700

    Made changes to make mlmd postgres compataible

--- cmflib/store/cmfstore.py
+++ cmflib/store/cmfstore.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 from ml_metadata.metadata_store import metadata_store
 
 class CmfStore(ABC):
- 
+
     def __init__(self, config):
         self.config = config
         super().__init__()
-    
+
     @abstractmethod
-    def connect(self)-> metadata_store.store:
+    def connect(self)-> metadata_store:
         cmf_store = metadata_store.MetadataStore(self.config)
         return cmf_store
