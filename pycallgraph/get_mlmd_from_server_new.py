7c3df123ed28b39c69196d4a5a513a7e74829ee9
AyeshaSanadi <ayesha.sanadi@agiliad.com>
2024-07-17 14:53:57 +0530

    Updated logic for get_mlmd_from_server function

--- pycallgraph/get_mlmd_from_server_new.py
+++ pycallgraph/get_mlmd_from_server_new.py
@@ -9,42 +9,37 @@ import json, glob
 import os
 
 
-def get_mlmd_from_server(server_store_path, pipeline_name, exec_id):
-        query = cmfquery.CmfQuery(server_store_path)
-        execution_flag = 0
-    # checks if given execution_id present in mlmd
-        if ( pipeline_name in query.get_pipeline_names() or exe_id == None):  # checks if pipeline name is available in mlmd
-            json_payload = query.dumptojson(pipeline_name, None)
-            mlmd_data = json.loads(json_payload)["Pipeline"]
-        elif (exe_id != None):
-            for stage in mlmd_data[0]["stages"]:
-                for execution in stage["executions"]:
-                    if execution["id"] == int(exec_id):
-                        execution_flag = 1
-                        break
-            if execution_flag == 1:
-                json_payload = query.dumptojson(pipeline_name, exec_id)
-            else:
+def get_mlmd_from_server(server_store_path: str, pipeline_name: str, exec_id: str):
+    query = cmfquery.CmfQuery(server_store_path)
+    execution_flag = 0
+    json_payload = None
+    df = pd.DataFrame()
+    if(query.get_pipeline_id(pipeline_name)!=-1):  # checks if pipeline name is available in mlmd
+        if exec_id != None:
+            exec_id = int(exec_id)
+            df = query.get_all_executions_by_ids_list([exec_id])
+            if df.empty:
                 json_payload = "no_exec_id"
                 return json_payload
-        else:
-            json_payload = "NULL"
-        return json_payload
+        json_payload = query.dumptojson(pipeline_name, exec_id)
+    return json_payload
 
 
 def get_mlmd() -> None:
     config = Config(max_depth=9)
     config.trace_filter = GlobbingFilter(exclude=["pycallgraph2.*", "selectors.*", "collections.*", "<module>", "email.*", "fsspec.*", "funcy.*", 
+                                                   "logging.*", "absl.*", "typing.*", "threading.*", "sre_compile.*", "sre_parse.*", "codecs.*",
                                                    "warnings.*", "os.*", "subprocess.*", "<lambda>", "encodings.*", "gzip.*", "genericpath.*",
                                                    "_process_posts", "enum.*", "uuid.*", "xml.*", "pandas.*", "numpy.*"])
     graphviz = GraphvizOutput(output_file='mlmd_from_server_new.png')
     with PyCallGraph(output=graphviz, config=config):
         mlmdfilePath="/home/ayesha/cmf-server/data/mlmd"
-        pipeline_name="Test-env"
-        jsonInfo = get_mlmd_from_server(mlmdfilePath, pipeline_name, None)
+        pipeline_name="Test-env1"
+        jsonInfo = get_mlmd_from_server(mlmdfilePath, pipeline_name, 6)
         print(jsonInfo)
 
 if __name__ == '__main__':
     get_mlmd()
+
 
 
--- server/app/get_data.py
+++ server/app/get_data.py
@@ -240,31 +240,21 @@ async def create_unique_executions(server_store_path, req_info):
     return status
 
 
-async def get_mlmd_from_server(server_store_path, pipeline_name, exec_id):
+async def get_mlmd_from_server(server_store_path: str, pipeline_name: str, exec_id: str):
     query = cmfquery.CmfQuery(server_store_path)
     execution_flag = 0
-    # checks if given execution_id present in mlmd
-    if (
-        pipeline_name in query.get_pipeline_names()
-    ):  # checks if pipeline name is available in mlmd
-        json_payload = query.dumptojson(pipeline_name, None)
-        mlmd_data = json.loads(json_payload)["Pipeline"]
-        if exec_id == None:
-            json_payload = query.dumptojson(pipeline_name, exec_id)
-        else:
-            for stage in mlmd_data[0]["stages"]:
-                for execution in stage["executions"]:
-                    if execution["id"] == int(exec_id):
-                        execution_flag = 1
-                        break
-            if execution_flag == 1:
-                json_payload = query.dumptojson(pipeline_name, exec_id)
-            else:
+    json_payload = None
+    df = pd.DataFrame()
+    if(query.get_pipeline_id(pipeline_name)!=-1):  # checks if pipeline name is available in mlmd
+        if exec_id != None:
+            exec_id = int(exec_id)
+            df = query.get_all_executions_by_ids_list([exec_id])
+            if df.empty:
                 json_payload = "no_exec_id"
                 return json_payload
-    else:
-        json_payload = "NULL"
+        json_payload = query.dumptojson(pipeline_name, exec_id)
     return json_payload
+
 
 async def get_lineage_data(server_store_path,pipeline_name,type,dict_of_art_ids,dict_of_exe_ids):
     query = cmfquery.CmfQuery(server_store_path)
