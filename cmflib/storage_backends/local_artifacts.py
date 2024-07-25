4025a62e6344eb9c49a22945bc09d28719f1cac6
Varkha Sharma <varkhadsharma@gmail.com>
2024-07-25 00:45:51 -0700

    making changes to fix artifact pull form amazonS3, local and minio repo

--- cmflib/storage_backends/local_artifacts.py
+++ cmflib/storage_backends/local_artifacts.py
@@ -22,7 +22,7 @@ class LocalArtifacts:
         self,
         dvc_config_op,
         current_directory: str,
-        current_loc: str,
+        object_name: str,
         download_loc: str,
     ):
         obj = True
@@ -30,46 +30,54 @@ class LocalArtifacts:
             fs = DVCFileSystem(
                 dvc_config_op["remote.local-storage.url"]
             )  # dvc_config_op[1] is file system path - "/path/to/local/repository"
+            
             # get_file() only creates file, to put artifacts in proper directory, subfolders are required.
             # download_loc = contains absolute path of the file with file name and extension
-            temp = download_loc.split("/")
-            temp.pop()
-            dir_path = "/".join(temp)
-            os.makedirs(dir_path, mode=0o777, exist_ok=True) # creating subfolders
-            if current_loc.endswith('.dir'):
+            dir_path = ""
+            if "/" in download_loc:
+                dir_path, _ = download_loc.rsplit("/", 1)
+            if dir_path != "":
+                os.makedirs(dir_path, mode=0o777, exist_ok=True)  # creating subfolders if needed
+
+            response = ""
+
+            if object_name.endswith('.dir'):
                 # in case of .dir, download_loc is a absolute path for a folder
                 os.makedirs(download_loc, mode=0o777, exist_ok=True)
-                # download the .dir file
+
+                # download the .dir object 
                 temp_dir = f"{download_loc}/dir"
-                fs.get_file(current_loc, temp_dir)
+                response = fs.get_file(object_name, temp_dir)
+
                 with open(temp_dir, 'r') as file:
                     tracked_files = eval(file.read())
+
+                # removing temp_dir
+                if os.path.exists(temp_dir):
+                    os.remove(temp_dir)
+
                 """
-                current_loc = "files/md5/9b/9a458ac0b534f088a47c2b68bae479.dir" 
+                object_name = "files/md5/9b/9a458ac0b534f088a47c2b68bae479.dir" 
                 contains the path of the .dir on the artifact repo
-                we need to remove the hash of the .dir from the current_loc 
+                we need to remove the hash of the .dir from the object_name
                 which will leave us with the artifact repo path
                 """
-                repo_path = current_loc.split("/")
-                repo_path = repo_path[:len(repo_path)-2]
-                repo_path = "/".join(repo_path)
+                repo_path = "/".join(object_name.split("/")[:-2])
                 for file_info in tracked_files:
                     relpath = file_info['relpath']
                     md5_val = file_info['md5']
                     # md5_val = a237457aa730c396e5acdbc5a64c8453
                     # we need a2/37457aa730c396e5acdbc5a64c8453
                     formatted_md5 = md5_val[:2] + '/' + md5_val[2:]
+                    temp_object_name = f"{repo_path}/{formatted_md5}"
                     temp_download_loc = f"{download_loc}/{relpath}"
-                    temp_current_loc = f"{repo_path}/{formatted_md5}"
-                    obj = fs.get_file(temp_current_loc, temp_download_loc)
+                    obj = fs.get_file(temp_object_name, temp_download_loc)
                     if obj == None: 
-                        print(f"object {temp_current_loc} downloaded at {temp_download_loc}.")
-                if os.path.exists(temp_dir):
-                    os.remove(temp_dir)
+                        print(f"object {temp_object_name} downloaded at {temp_download_loc}.")
             else:
-                obj = fs.get_file(current_loc, download_loc)
-            if obj == None:  # get_file() returns none when file gets downloaded.
-                stmt = f"object {current_loc} downloaded at {download_loc}."
+                response = fs.get_file(object_name, download_loc)
+            if response == None:  # get_file() returns none when file gets downloaded.
+                stmt = f"object {object_name} downloaded at {download_loc}."
                 return stmt
         except TypeError as exception:
             return exception
