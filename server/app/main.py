a5c56fe1f1ea7afeb9ace72c292e378ce5b35cb8
abhinavchobey <111754147+abhinavchobey@users.noreply.github.com>
2024-08-29 06:50:43 +0530

    Regression master branch (#203)
    * bug fix in update_global_exe_ids
    
    * bug update_global_art_ids updates doesn't modifies
    
    * adding changes
    
    * Added logging dir before current script
    
    ---------
    
    Co-authored-by: Abhinav Chobey <chobey@varkha-test-cors-machine.labs.hpecorp.net>
    Co-authored-by: AyeshaSanadi <ayesha.sanadi@agiliad.com>

--- server/app/main.py
+++ server/app/main.py
@@ -373,18 +373,14 @@ async def model_card(request:Request, modelId: int, response_model=List[Dict[str
 async def update_global_art_dict(pipeline_name):
     global dict_of_art_ids
     output_dict = await get_all_artifact_ids(server_store_path, dict_of_exe_ids, pipeline_name)
-    if pipeline_name in dict_of_art_ids:
-        dict_of_art_ids[pipeline_name].update(output_dict[pipeline_name])
-    else:
-        dict_of_art_ids[pipeline_name] = output_dict[pipeline_name]
+    # type(dict_of_exe_ids[pipeline_name]) = Dict[ <class 'pandas.core.frame.DataFrame'> ]
+    dict_of_art_ids[pipeline_name]=output_dict[pipeline_name]
     return
 
 
 async def update_global_exe_dict(pipeline_name):
     global dict_of_exe_ids
     output_dict = await get_all_exe_ids(server_store_path, pipeline_name)
-    if pipeline_name in dict_of_exe_ids:
-        dict_of_exe_ids[pipeline_name].update(output_dict[pipeline_name])
-    else:
-        dict_of_exe_ids[pipeline_name] = output_dict[pipeline_name]
+    # type(dict_of_exe_ids[pipeline_name]) = <class 'pandas.core.frame.DataFrame'>
+    dict_of_exe_ids[pipeline_name] = output_dict[pipeline_name]  
     return
