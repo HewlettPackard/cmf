7d90bee570e5278f8950cb73aad2ffcb72340da1
rishabhsharma22 <rishabh230795@gmail.com>
2024-10-02 11:34:13 -0700

    model and dataset logging for raytune using CMF

--- cmflib/cmf_ray_logger.py
+++ cmflib/cmf_ray_logger.py
@@ -5,7 +5,7 @@ from cmflib import cmf
 class CmfRayLogger(Callback):
     #id_count = 1
     
-    def __init__(self, pipeline_name, file_path, pipeline_stage):
+    def __init__(self, pipeline_name, file_path, pipeline_stage, data_dir = None):
         """
         pipeline_name: The name of the CMF Pipelibe
         file_path: The path to metadata file
@@ -15,23 +15,29 @@ class CmfRayLogger(Callback):
         self.file_path = file_path
         self.pipeline_stage = pipeline_stage
         self.cmf_obj = {}
+        self.cmf_run = {}
+        self.data_dir = data_dir
 
     def on_trial_start(self, iteration, trials, trial, **info):
         trial_id = trial.trial_id
+        trial_config = trial.config
         print(f"CMF Logging Started for Trial {trial_id}")
         self.cmf_obj[trial_id] = cmf.Cmf(filepath = self.file_path, pipeline_name = self.pipeline_name)
         _ = self.cmf_obj[trial_id].create_context(pipeline_stage = self.pipeline_stage)
         _ = self.cmf_obj[trial_id].create_execution(execution_type=f"Trial_{trial_id}",
-                                            create_new_execution = False)
+                                            create_new_execution = False,
+                                            custom_properties = {'Configuration': trial_config})
         #self.execution_id[trial_id] = CmfRayLogger.id_count
         #CmfRayLogger.id_count+=1
+        if self.data_dir:
+            _ = self.cmf_obj[trial_id].log_dataset(url = str(self.data_dir), event = "input")
 
     def on_trial_result(self, iteration, trials, trial, result, **info):
         trial_id = trial.trial_id
         trial_config = trial.config
         trial_result = trial.last_result
         curr_res = result
-        print(f'In Trial Results')
+        #print(f'In Trial Results')
         print(f'Trial_ID: {trial_id}')
         print(f'curr_results is {curr_res}')
         print(f'Logging results to CMF with metric name Trial_{trial_id}_metrics')
@@ -39,6 +45,7 @@ class CmfRayLogger(Callback):
         #    _ = self.metawriter.update_execution(int(self.execution_id[trial_id]))
         _ = self.cmf_obj[trial_id].log_metric(metrics_name = f"Trial_{trial_id}_metrics",
                                       custom_properties = {'Output': curr_res})
+        self.cmf_run[trial_id] = True
         
         
     def on_trial_complete(self, iteration, trials, trial, **info):
@@ -51,6 +58,13 @@ class CmfRayLogger(Callback):
         #if trial_id in self.execution_id:
         #    _ = self.metawriter.update_execution(int(self.execution_id[trial_id]))
         _ = self.cmf_obj[trial_id].commit_metrics(f"Trial_{trial_id}_metrics")
+        _ = self.cmf_obj[trial_id].log_execution_metrics(metrics_name = f"Trial_{trial_id}_Result",
+                                      custom_properties = {'Result': trial_result})
+        
+        if 'model_path' in trial_result:
+            _ = self.cmf_obj[trial_id].log_model(path = trial_result['model_path'], 
+                                                 event = 'input', 
+                                                 model_name = f"{trial_id}_model")
         
     def on_trial_error(self, iteration, trials, trial, **info):
         trial_id = trial.trial_id
@@ -58,3 +72,7 @@ class CmfRayLogger(Callback):
         trial_result = trial.last_result
 
         print(f"An error occured with Trial {trial_id}, Not commiting anything to cmf")
+        if self.cmf_run[trial_id]:
+            _ = self.cmf_obj[trial_id].commit_metrics(f"Trial_{trial_id}_metrics")
+            _ = self.cmf_obj[trial_id].log_execution_metrics(metrics_name = f"Trial_{trial_id}_Result",
+                                      custom_properties = {'Result': '-inf'})
