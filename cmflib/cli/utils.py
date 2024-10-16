a6b005ab88dbb7f38a81feac3542d1229e25ce54
Varkha Sharma <varkhadsharma@gmail.com>
2024-10-16 01:50:20 -0700

    added comments and enhanced code for ssh artifact pull

--- cmflib/cli/utils.py
+++ cmflib/cli/utils.py
@@ -57,10 +57,12 @@ def check_minio_server(dvc_config_op):
     from minio.error import S3Error
 
     if dvc_config_op["core.remote"] == "minio":
-        endpoint = dvc_config_op["remote.minio.endpointurl"][:7]   # http:// has 7 total no of character so we are using slicing 
+        # dvc_config_op["remote.minio.endpointurl"] = http://XX.XX.XX.XX:9000 
+        endpoint = dvc_config_op["remote.minio.endpointurl"].split("http://")[1]
         access_key = dvc_config_op["remote.minio.access_key_id"]
         secret_key = dvc_config_op["remote.minio.secret_access_key"]
-        bucket_name = dvc_config_op["remote.minio.url"][:5]  # s3:// has 5 total no of character so we are using slicing
+        # dvc_config_op["remote.minio.url"] = s3://dvc-art
+        bucket_name = dvc_config_op["remote.minio.url"].split("s3://")[1]
         try:
             client = Minio(
                 endpoint, access_key=access_key, secret_key=secret_key, secure=False
