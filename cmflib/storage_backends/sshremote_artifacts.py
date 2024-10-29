175db857aa7d4beb6d4c76917037f43fa5baa49e
Varkha Sharma <112053040+varkha-d-sharma@users.noreply.github.com>
2024-10-30 03:47:16 +0530

    making changes to 'cmf artifact push' to include external artifacts (#215)
    * making changes to 'cmf artifact push' to include external artifacts
    
    * removed print statments
    
    * resolved 'cmf artifact pull' command for sshremote repo

--- cmflib/storage_backends/sshremote_artifacts.py
+++ cmflib/storage_backends/sshremote_artifacts.py
@@ -30,7 +30,6 @@ class SSHremoteArtifacts:
         object_name: str,
         download_loc: str,
     ):
-        remote_repo = dvc_config_op["remote.ssh-storage.url"]
         user = dvc_config_op["remote.ssh-storage.user"]
         password = dvc_config_op["remote.ssh-storage.password"]
         try:
@@ -40,18 +39,19 @@ class SSHremoteArtifacts:
             )  # this can lead to man in the middle attack, need to find another solution
             ssh.connect(host, username=user, password=password)
             sftp = ssh.open_sftp()
-
             dir_path = ""
+            # in case download_loc is absolute path like home/user/test/data.xml.gz
+            # we need to make this absolute path a relative one by removing first '/'
+            if os.path.isabs(download_loc):
+                download_loc = download_loc[1:]
             if "/" in download_loc:
                 dir_path, _ = download_loc.rsplit("/", 1)
             if dir_path != "":
-                # creates subfolders needed as per artifacts folder structure
-                os.makedirs(dir_path, mode=0o777, exist_ok=True)  
+                # creates subfolders needed as per artifacts' folder structure
+                os.makedirs(dir_path, mode=0o777, exist_ok=True) 
 
             response = ""
-
             abs_download_loc = os.path.abspath(os.path.join(current_directory, download_loc))
-
             """"
             if object_name ends with .dir - it is a directory.
             we download .dir object with 'temp_dir' and remove 
@@ -93,7 +93,6 @@ class SSHremoteArtifacts:
                         print(f"object {temp_object_name} downloaded at {temp_download_loc}.")
             else:
                 response = sftp.put(object_name, abs_download_loc)
-            
             if response:
                 stmt = f"object {object_name} downloaded at {abs_download_loc}."
                 return stmt
