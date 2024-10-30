77e29a377cc7f7e550ae800604396607bc12f54c
abhinavchobey <111754147+abhinavchobey@users.noreply.github.com>
2024-10-30 18:05:26 +0530

    Gui enhancement (#217)
    * gui enhancement for look and feel improvement
    
    * formatted all ui files with prettier
    
    * updates
    
    * Added date and time column, modify sorting icons and removed url column from metrics
    
    * removed libzbar pythond3dev install from Dockerfile
    
    * centered textalign in home and added hover in artifacts and execution page
    
    * Update index.jsx
    
    * Added UTC date and time
    
    * lineage changes added to resolve merge conflict and pagination changes in artifact/index.jsx
    
    ---------
    
    Co-authored-by: First Second <first.second@corp.org>
    Co-authored-by: AyeshaSanadi <ayesha.sanadi@agiliad.com>

--- ui/src/pages/tensorboard/index.jsx
+++ ui/src/pages/tensorboard/index.jsx
@@ -1,19 +1,18 @@
 /***
-* Copyright (2023) Hewlett Packard Enterprise Development LP
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* You may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-***/
-
+ * Copyright (2023) Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***/
 
 import React from "react";
 import config from "../../config";
@@ -21,8 +20,7 @@ import DashboardHeader from "../../components/DashboardHeader";
 import Footer from "../../components/Footer";
 
 const TensorBoard = () => {
-
-  const env = config.apiBasePathWOPort + ":6006"
+  const env = config.apiBasePathWOPort + ":6006";
 
   return (
     <>
@@ -37,7 +35,8 @@ const TensorBoard = () => {
               title="tensorboard Iframe"
               src={env}
               allowFullScreen
-              width="100%" height="1200"
+              width="100%"
+              height="1200"
             />
           </div>
         </div>
