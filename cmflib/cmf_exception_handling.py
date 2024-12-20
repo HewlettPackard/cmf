e2ec3fd481ac8f315a16d1a53ecee2adc3bf7563
Varkha Sharma <varkhadsharma@gmail.com>
2024-12-20 04:47:21 -0800

    addressed review comments

--- cmflib/cmf_exception_handling.py
+++ cmflib/cmf_exception_handling.py
@@ -17,42 +17,48 @@
 #!/usr/bin/env python3
 from typing import Optional, List
 
+
 class CmfResponse(Exception):
     """
-        Response and Exceptions raised by the CMF.
-        CmfResponse includes two child classes, 
-            1. CmfSuccess
-            2. CmfFailure
-        On the basis of success and failure various child classes are created.
+    Response and Exceptions raised by the CMF.
+    CmfResponse includes two child classes,
+        1. CmfSuccess
+        2. CmfFailure
+    On the basis of success and failure various child classes are created.
 
-        Base class for all the cmf responses and  exceptions.
+    Base class for all the cmf responses and  exceptions.
     """
 
-    def __init__(self, return_code=None,  status="failure", *args):
+    def __init__(self, return_code=None, status="failure", *args):
         self.return_code = return_code
         self.status = status
         super().__init__(*args)
 
+
 class CmfFailure(CmfResponse):
     def __init__(self, return_code=None, *args):
         super().__init__(return_code, status="failure", *args)
+
 
 # Subclass for Success Cases
 class CmfSuccess(CmfResponse):
     def __init__(self, return_code=None, *args):
         super().__init__(return_code, status="success", *args)
 
-'''CMF Success Class'''
+
+"""CMF Success Class"""
+
 
 class ExecutionsAlreadyExists(CmfSuccess):
     def __init__(self, return_code=201):
         super().__init__(return_code)
- 
+
     def handle(self):
         return "INFO: Executions already exists."
 
+
 class ObjectDownloadSuccess(CmfSuccess):
-    def __init__(self,object_name,download_loc, return_code=202):
+    def __init__(self, object_name, download_loc, return_code=202):
         self.object_name = object_name
         self.download_loc = download_loc
         super().__init__(return_code)
@@ -60,39 +66,46 @@ class ObjectDownloadSuccess(CmfSuccess):
     def handle(self):
         return f"SUCCESS: Object {self.object_name} downloaded at {self.download_loc}."
 
+
 class BatchDownloadSuccess(CmfSuccess):
-    def __init__(self,files_downloaded, return_code=203):
+    def __init__(self, files_downloaded, return_code=203):
         self.files_downloaded = files_downloaded
         super().__init__(return_code)
 
     def handle(self):
         return f"SUCCESS: Number of files downloaded = {self.files_downloaded }."
 
+
 class MlmdFilePullSuccess(CmfSuccess):
-    def __init__(self,full_path_to_dump, return_code=204):
+    def __init__(self, full_path_to_dump, return_code=204):
         self.full_path_to_dump = full_path_to_dump
         super().__init__(return_code)
 
     def handle(self):
         return f"SUCCESS: {self.full_path_to_dump} is successfully pulled."
-    
+
+
 class MlmdFilePushSuccess(CmfSuccess):
     def __init__(self, file_name, return_code=205):
         self.file_name = file_name
         super().__init__(return_code)
-    
+
     def handle(self):
         return f"SUCCESS: {self.file_name} is successfully pushed."
 
+
 class TensorboardPushSuccess(CmfSuccess):
-    def __init__(self, tensorboard_file_name:str = "All", return_code=206):
+    def __init__(self, tensorboard_file_name: str = "All", return_code=206):
         self.tensorboard_file_name = tensorboard_file_name
         super().__init__(return_code)
 
     def handle(self):
         if self.tensorboard_file_name == "All":
-            return f"SUCCESS: All tensorboard logs pushed successfully." 
-        return f"tensorboard logs: file {self.tensorboard_file_name} pushed successfully." 
+            return f"SUCCESS: All tensorboard logs pushed successfully."
+        return (
+            f"tensorboard logs: file {self.tensorboard_file_name} pushed successfully."
+        )
+
 
 class CmfInitComplete(CmfSuccess):
     def __init__(self, return_code=207):
@@ -101,14 +114,16 @@ class CmfInitComplete(CmfSuccess):
     def handle(self):
         return "SUCCESS: cmf init complete."
 
+
 class CmfInitShow(CmfSuccess):
-    def __init__(self,result, attr_str, return_code=208):
+    def __init__(self, result, attr_str, return_code=208):
         self.result = result
         self.attr_str = attr_str
         super().__init__(return_code)
 
     def handle(self):
         return f"{self.result}\n{self.attr_str}"
+
 
 class ArtifactPushSuccess(CmfSuccess):
     def __init__(self, message, return_code=209):
@@ -118,17 +133,24 @@ class ArtifactPushSuccess(CmfSuccess):
     def handle(self):
         return self.message
 
+
 class MetadataExportToJson(CmfSuccess):
-    def __init__(self,full_path_to_dump, return_code=210):
+    def __init__(self, full_path_to_dump, return_code=210):
         self.full_path_to_dump = full_path_to_dump
         super().__init__(return_code)
 
     def handle(self):
         return f"SUCCESS: metadata successfully exported in {self.full_path_to_dump}."
 
+
 # This class is created for messages like "Done", "Records not found"
 class MsgSuccess(CmfSuccess):
-    def __init__(self,msg_str: Optional[str] = None, msg_list: Optional[List[str]] = None,  return_code=211):
+    def __init__(
+        self,
+        msg_str: Optional[str] = None,
+        msg_list: Optional[List[str]] = None,
+        return_code=211,
+    ):
         self.msg_str = msg_str
         self.msg_list = msg_list
         super().__init__(return_code)
@@ -139,67 +161,75 @@ class MsgSuccess(CmfSuccess):
         else:
             return self.msg_str
 
-''' CMF FAILURE CLASSES'''
+
+""" CMF FAILURE CLASSES"""
+
 
 class PipelineNotFound(CmfFailure):
-    def __init__(self,pipeline_name,return_code=101):
-        self.pipeline_name = pipeline_name 
+    def __init__(self, pipeline_name, return_code=101):
+        self.pipeline_name = pipeline_name
         super().__init__(return_code)
 
     def handle(self):
         return f"ERROR: Pipeline name {self.pipeline_name} doesn't exist."
 
-    
+
 class FileNotFound(CmfFailure):
     def __init__(self, file_name, directory, return_code=102):
         self.directory = directory
-        self.file_name =file_name
+        self.file_name = file_name
         super().__init__(return_code)
-    
+
     def handle(self):
         return f"ERROR: File {self.file_name} doesn't exists in {self.directory} directory."
 
+
 class BucketNotFound(CmfFailure):
-    def __init__(self,bucket_name, return_code=103):
+    def __init__(self, bucket_name, return_code=103):
         self.bucket_name = bucket_name
         super().__init__(return_code)
- 
+
     def handle(self):
         return f"ERROR: Bucket {self.bucket_name} doesn't exist."
+
 
 class ExecutionsNotFound(CmfFailure):
     def __init__(self, return_code=104):
         super().__init__(return_code)
- 
+
     def handle(self):
         return f"ERROR: Executions not found."
-    
+
+
 class ExecutionIDNotFound(CmfFailure):
-    def __init__(self,exec_id, return_code=105):
+    def __init__(self, exec_id, return_code=105):
         self.exec_id = exec_id
         super().__init__(return_code)
- 
+
     def handle(self):
         return f"ERROR: Execution id {self.exec_id} is not present in mlmd."
-    
+
+
 class ArtifactNotFound(CmfFailure):
-    def __init__(self,artifact_name, return_code=106):
+    def __init__(self, artifact_name, return_code=106):
         self.artifact_name = artifact_name
         super().__init__(return_code)
- 
+
     def handle(self):
         return f"ERROR: Artifact {self.artifact_name} not found."
-    
+
+
 class ObjectDownloadFailure(CmfFailure):
-    def __init__(self,object_name, return_code=107):
+    def __init__(self, object_name, return_code=107):
         self.object_name = object_name
         super().__init__(return_code)
 
     def handle(self):
         return f"Object {self.object_name} is not downloaded."
-    
+
+
 class BatchDownloadFailure(CmfFailure):
-    def __init__(self,files_downloaded, Files_failed_to_download, return_code=108):
+    def __init__(self, files_downloaded, Files_failed_to_download, return_code=108):
         self.files_downloaded = files_downloaded
         self.Files_failed_to_download = Files_failed_to_download
         super().__init__(return_code)
@@ -207,20 +237,23 @@ class BatchDownloadFailure(CmfFailure):
     def handle(self):
         return f"INFO: Number of files downloaded = {self.files_downloaded }. Files failed to download = {self.Files_failed_to_download}."
 
+
 class Minios3ServerInactive(CmfFailure):
-    def __init__(self,return_code=109):
+    def __init__(self, return_code=109):
         super().__init__(return_code)
 
     def handle(self):
         return f"ERROR: MinioS3 server is not running!!!"
 
+
 class CmfNotConfigured(CmfFailure):
-    def __init__(self,message, return_code=110):
+    def __init__(self, message, return_code=110):
         self.message = message
         super().__init__(return_code)
 
     def handle(self):
         return self.message
+
 
 class MlmdNotFoundOnServer(CmfFailure):
     def __init__(self, return_code=111):
@@ -229,6 +262,7 @@ class MlmdNotFoundOnServer(CmfFailure):
     def handle(self):
         return "ERROR: Metadata file not available on cmf-server."
 
+
 class UpdateCmfVersion(CmfFailure):
     def __init__(self, return_code=112):
         super().__init__(return_code)
@@ -236,14 +270,16 @@ class UpdateCmfVersion(CmfFailure):
     def handle(self):
         return "ERROR: You need to update cmf to the latest version. Unable to push metadata file."
 
+
 class TensorboardPushFailure(CmfFailure):
-    def __init__(self,tensorboard_file_name,response_text, return_code=113):
+    def __init__(self, tensorboard_file_name, response_text, return_code=113):
         self.tensorboard_file_name = tensorboard_file_name
         self.response_text = response_text
         super().__init__(return_code)
 
     def handle(self):
         return f"ERROR: Failed to upload file {self.tensorboard_file_name}. Server response: {self.response_text}."
+
 
 class Neo4jArgumentNotProvided(CmfFailure):
     def __init__(self, return_code=114):
@@ -252,12 +288,14 @@ class Neo4jArgumentNotProvided(CmfFailure):
     def handle(self):
         return "ERROR: Provide user, password and uri for neo4j initialization."
 
+
 class CmfInitFailed(CmfFailure):
     def __init__(self, return_code=115):
         super().__init__(return_code)
 
     def handle(self):
         return "ERROR: cmf init failed."
+
 
 class CmfServerNotAvailable(CmfFailure):
     def __init__(self, return_code=116):
@@ -266,12 +304,14 @@ class CmfServerNotAvailable(CmfFailure):
     def handle(self):
         return "ERROR: cmf-server is not available."
 
+
 class InternalServerError(CmfFailure):
     def __init__(self, return_code=117):
         super().__init__(return_code)
 
     def handle(self):
         return "cmf-server error: The server encountered an unexpected error."
+
 
 class MlmdFilePullFailure(CmfFailure):
     def __init__(self, return_code=118):
@@ -280,13 +320,15 @@ class MlmdFilePullFailure(CmfFailure):
     def handle(self):
         return "ERROR: Unable to pull metadata file."
 
+
 class DirectoryNotfound(CmfFailure):
-    def __init__(self,dir, return_code=119):
+    def __init__(self, dir, return_code=119):
         self.dir = dir
         super().__init__(return_code)
 
     def handle(self):
         return f"ERROR: {self.dir} doesn't exists."
+
 
 class FileNameNotfound(CmfFailure):
     def __init__(self, return_code=120):
@@ -295,12 +337,14 @@ class FileNameNotfound(CmfFailure):
     def handle(self):
         return "ERROR: Provide path with file name."
 
+
 class NoDataFoundOsdf(CmfFailure):
     def __init__(self, return_code=121):
         super().__init__(return_code)
 
     def handle(self):
         return "ERROR: No data received from the server."
+
 
 class InvalidTensorboardFilePath(CmfFailure):
     def __init__(self, return_code=122):
@@ -309,8 +353,9 @@ class InvalidTensorboardFilePath(CmfFailure):
     def handle(self):
         return "ERROR: Invalid tensorboard logs path. Provide valid file/folder path for tensorboard logs!!"
 
+
 class DuplicateArgumentNotAllowed(CmfFailure):
-    def __init__(self,argument_name, argument_flag, return_code=123):
+    def __init__(self, argument_name, argument_flag, return_code=123):
         self.argument_flag = argument_flag
         self.argument_name = argument_name
         super().__init__(return_code)
@@ -318,23 +363,31 @@ class DuplicateArgumentNotAllowed(CmfFailure):
     def handle(self):
         return f"Error: You can only provide one {self.argument_name} using the {self.argument_flag} flag."
 
+
 class MissingArgument(CmfFailure):
-    def __init__(self,argument_name,return_code=124):
+    def __init__(self, argument_name, return_code=124):
         self.argument_name = argument_name
         super().__init__(return_code)
 
     def handle(self):
         return f"Error: Missing {self.argument_name}"
 
+
 class NoChangesMadeInfo(CmfFailure):
-    def __init__(self,return_code=125):
+    def __init__(self, return_code=125):
         super().__init__(return_code)
 
     def handle(self):
         return "INFO: No changes made to the file. Operation aborted."
 
+
 class MsgFailure(CmfFailure):
-    def __init__(self,msg_str: Optional[str] = None, msg_list: Optional[List[str]] = None,  return_code=126):
+    def __init__(
+        self,
+        msg_str: Optional[str] = None,
+        msg_list: Optional[List[str]] = None,
+        return_code=126,
+    ):
         self.msg_str = msg_str
         self.msg_list = msg_list
         super().__init__(return_code)
